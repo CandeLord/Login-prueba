71c3805e9b4ae4a2844a75227516b58020f55667
Cande Lord <candelord93@gmail.com>
2023-03-30 23:51:10 -0300

    Recuperamos elementos del HTML/ Creación de localStorage

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+//#region LOGIN
 const submit = document.getElementById("submit");
 const inputMail = document.getElementById("inputmail");
 const inputPassword = document.getElementById("inputpass");
+
+
+submit.addEventListener("click", function(e){
+    e.preventDefault();
+});
+inputMail.addEventListener("change", function(e){
+    mail = e.target.value;
+});
+inputPassword.addEventListener("change", function(e){
+   password = e.target.value;
+})
+
+//#endregion
+//#region CREACIÓN USUARIO
+const saveChanges = document.getElementById("btnSave");
+const inputCreateMail = document.getElementById("inputcreatemail");
+const inputCreatePassword = document.getElementById("createpass");
+const inputCreateRePassword = document.getElementById("createrepeatpass");
+
+let createMail = "";
+let createPassword ="";
+let createRePassword ="";
+
+saveChanges.addEventListener("click", function(e){
+    e.preventDefault();
+    console.log(createMail, createPassword, createRePassword);
+});
+inputCreateMail.addEventListener("change", function(e){
+    createMail = e.target.value;
+ })
+inputCreatePassword.addEventListener("change", function(e){
+    createPassword = e.target.value;
+});
+inputCreateRePassword.addEventListener("change", function(e){
+   createRePassword = e.target.value;
+})
+//#endregion
+let mail = "";
+let password ="";
